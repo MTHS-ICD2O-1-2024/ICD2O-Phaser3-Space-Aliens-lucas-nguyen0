39f46be9123b8d81dcd7dbd3afe4281f8293762e
lucas-nguyen0 <lucas.nguyen@mths.ca>
2025-04-11 17:08:31 +0000

    game changes in config

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2025 Lucas Nguyen All rights reserved
+//
+// Created by: Lucas Nguyen
+// Created on: Apr 2025
+// This is the Phaser3 game configuration file
+
+/**
+ *Start Phaser Gamer.
+ */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
