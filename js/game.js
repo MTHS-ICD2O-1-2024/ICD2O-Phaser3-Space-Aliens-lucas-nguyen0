37b76d74deb979ae5652605450b041dab33a4c5e
lucas-nguyen0 <lucas.nguyen@mths.ca>
2025-04-14 17:20:26 +0000

    standard changes

--- js/game.js
+++ js/game.js
@@ -10,23 +10,23 @@
  *Start Phaser Gamer.
  */
 
-"use strict"
+'use strict'
 
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
-      debug: true,
-    },
+      debug: true
+    }
   },
   backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
